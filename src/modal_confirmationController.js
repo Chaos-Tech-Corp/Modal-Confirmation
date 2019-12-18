b4126f97af02fae6853420d4282b29452c73bcd5
Javier Chaos <jachaos@gmail.com>
2019-12-18 10:57:27 -0500

    Update modal_confirmationController.js

--- src/modal_confirmationController.js
+++ src/modal_confirmationController.js
@@ -2,11 +2,15 @@
 	handleCancelClick : function(component, event, helper) {
 		component.destroy();
 	},
-    
-    handleOkClick : function(component, event, helper) {
-        //fire the confirmation event
-        component.getEvent("confirm").fire();
-        //destroy the component
+
+	handleOkClick : function(component, event, helper) {
+
+		//fire the confirmation event
+		var confirmEvent = component.getEvent("confirm");
+		confirmEvent.setParam("param", component.get("v.param"));
+		confirmEvent.fire();
+
+		//destroy the component
 		component.destroy();
 	}
 })
